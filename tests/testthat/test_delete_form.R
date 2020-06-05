3ba97c8525f5710002c89a81d2f754f6376617fc
jaybee84 <jinetab@gmail.com>
2020-06-05 15:49:09 -0700

    simplifying test, expanding test coverage

--- tests/testthat/test_delete_form.R
+++ tests/testthat/test_delete_form.R
@@ -1,33 +1,24 @@
-#' Delete a form
-#'
-#' Delete a form from the database, and caller must
-#' have SUBMIT privileges in formGroup ACL.
-#'
-#' @export
-#' @inheritParams get_submissions_metadata
-#' @inheritParams submit_form_for_review
-#' @return None
-#'
-# delete_form <- function(syn, form_data_id) {
-#   uri <- glue::glue("https://repo-prod.prod.sagebase.org/repo/v1/form/data/{form_data_id}") # nolint
-#   rest_delete(syn = syn, uri = uri)
-# }
-
-
 library(testthat)
 library(mockery)
 
+context("add-update-delete-forms.R")
+
 test_that("delete_form deletes the correct form", {
 
-  m <- mock(TRUE)
+  m <- mock(TRUE, cycle=TRUE)
   stub(delete_form, "rest_delete", m)
-  result <- delete_form(syn = "", form_data_id = 1)
-  expected_uri <- "https://repo-prod.prod.sagebase.org/repo/v1/form/data/1"
-  expect_args(m, 1, "", expected_uri)
-  expect_true(result)
 
-})
+  #Test coverage:
+  #true cases
+  expect_true(delete_form("", 1))
+  expect_true(delete_form("", 12))
+  expect_true(delete_form("", 10000))
 
-## Q1. how can I increase test coverage?  i.e. test for "x" being an invalid input (#for (i in c(1,2,4,"x")))
-## Q2. how is this different from testing if glue is working correctly?
-
+  #false cases
+  expect_error(delete_form("", 0))
+  expect_error(delete_form("", "x"))
+  expect_error(delete_form("", -1))
+  expect_error(delete_form("", "syn123"))
+  expect_error(delete_form("", Inf))
+  expect_error(delete_form("", ""))
+  })
